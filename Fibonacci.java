5bc9fd9a77a60c31912f9f647cdf30f002b87a3d
That_One_Nerd <dev@that-one-nerd.net>
2024-09-20 20:02:01 -0400

    Java template created.

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+// You edit this file. Both methods need to exist and be valid,
+// but other than that you have complete control of this file.
+//
+// All methods are included in the timer.
+public class Fibonacci
+{
+    // Invoked when the test first starts.
+    public void testStart() { }
+
+    // Invoked sequentially.
+    public BigInteger calculate(int num)
+    {
+        return BigInteger.ZERO;
+    }
+}
