4f2413369c846093a6366a5d949b940d66efc950
That-One-Nerd <crazymailbro2@yahoo.com>
2024-09-20 23:54:17 -0400

    F# template created.

---
+++ Fibonacci.fs
@@ -0,0 +1,20 @@
+﻿module Fibonacci
+
+open System.Numerics
+
+// You edit this file. Both methods need to exist and be valid,
+// but other than that you have complete control of this file.
+//
+// All methods are included in the timer.
+
+// F#-specific notice: You can mark either function as recursive
+//                     if need be.
+
+
+// Invoked when the test first starts.
+let testStart() = 
+    ignore()
+
+// Invoked sequentially.
+let calculate(num: int): BigInteger =
+    BigInteger(0)
